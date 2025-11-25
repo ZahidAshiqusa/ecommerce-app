aad9a35525c51a56433644707fa00f60b2b654f3
ZahidAshiqusa <zahidashiqusa@gmail.com>
2025-11-25 19:13:37 +0500

    Update Contact.jsx

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -19,19 +19,19 @@ const Contact = () => {
         <div className="flex flex-col justify-center items-start gap-6">
           <p className="font-semibold text-xl text-gray-600">Our Store</p>
           <p className="text-gray-500">
-            54709 willms Station <br /> Suite 350, Washington, USA
+            Near High School No.2 <br /> Hasilpur, Punjab, Pakistan
           </p>
           <p className="text-gray-500">
-            Tel: (415) 555-0132 <br /> Email: admin@forever.com
+            Tel: +92326-1067710 <br /> Email: zahidashiqusa@gmail.com
           </p>
           <p className="font-semibold text-xl text-gray-600">
-            Careers at Forever
+            Careers at AD7Store
           </p>
           <p className="text-gray-500">
-            Learn more about our teams and job openings.
+            
           </p>
           <button className="border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500">
-            Explore Jobs
+            
           </button>
         </div>
       </div>
